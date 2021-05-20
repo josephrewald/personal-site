61fa669f081ddc47fe77cb5911f0f88fb24dd3ef
Joseph Rewald <joseph.rewald@gmail.com>
2021-05-20 21:37:08 +0000

    Regular commit
    Resume complete

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -19,14 +19,9 @@ const SideBar = () => (
 
     <section className="blurb">
       <h2>About</h2>
-      <p>Hi, I&apos;m Michael. I like building things.
-        I am a <a href="https://icme.stanford.edu/">Stanford ICME</a> graduate, YC Alumni, and
-        the co-founder and CTO of <a href="https://arthena.com">Arthena</a>. Before Arthena I was
-        at <a href="https://matroid.com">Matroid</a>
-        , <a href="https://planet.com">Planet</a>
-        , <a href="https://planetaryresources.com">Planetary Resources</a>
-        , <a href="https://facebook.com">Facebook</a>
-        , and <a href="https://seds.org">SEDS</a>.
+      <p>
+        I&apos;m Joseph - a mechanical engineer with experience in power generation, storage and
+        handling of chemicals, wastewater treatment and more.
       </p>
       <ul className="actions">
         <li>
@@ -37,7 +32,7 @@ const SideBar = () => (
 
     <section id="footer">
       <ContactIcons />
-      <p className="copyright">&copy; Michael D&apos;Angelo <Link to="/">mldangelo.com</Link>.</p>
+      <p className="copyright">&copy; Joseph Rewald <Link to="/">rewald.engineer</Link>.</p>
     </section>
   </section>
 );
