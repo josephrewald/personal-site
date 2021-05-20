a45ea5b55870265f5df1cee27857717b0463bf5d
Joseph Rewald <joseph.rewald@gmail.com>
2021-05-20 02:54:03 +0000

    First minor changes

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -12,8 +12,8 @@ const SideBar = () => (
         <img src={`${PUBLIC_URL}/images/me.jpg`} alt="" />
       </Link>
       <header>
-        <h2>Michael D&apos;Angelo</h2>
-        <p><a href="mailto:michael.l.dangelo@gmail.com">michael.l.dangelo@gmail.com</a></p>
+        <h2>Joseph Rewald</h2>
+        <p><a href="mailto:joseph.rewald@gmail.com" target="_blank" rel="noopener noreferrer">joseph.rewald@gmail.com</a></p>
       </header>
     </section>
 
