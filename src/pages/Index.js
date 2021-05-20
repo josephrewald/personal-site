82dc195d615ac47c831638304353ff80f6009018
Joseph Rewald <joseph.rewald@gmail.com>
2021-05-20 18:18:37 +0000

    Regular commit.
    Completed step 3 of adapting guide and started step 4.

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -5,27 +5,27 @@ import Main from '../layouts/Main';
 
 const Index = () => (
   <Main
-    description={"Michael D'Angelo's personal website. New York based Stanford ICME graduate, "
-    + 'co-founder and CTO of Arthena, and YC Alumni.'}
+    description={"Joseph Rewald's personal website. Mechanical engineer and technology enthusiast. "}
+    // + 'co-founder and CTO of Arthena, and YC Alumni.'}
   >
     <article className="post" id="index">
       <header>
         <div className="title">
-          <h2 data-testid="heading"><Link to="/">About this site</Link></h2>
+          <h2 data-testid="heading"><Link to="/">Welcome to my site</Link></h2>
           <p>
-            A beautiful, responsive, statically-generated,
-            react application written with modern Javascript.
+            A place to share things about me, my background, my skills and,
+            most importantly, my dreams for the future!
           </p>
         </div>
       </header>
-      <p> Welcome to my website. Please feel free to read more <Link to="/about">about me</Link>,
+      <p> Please feel free to read more <Link to="/about">about me</Link>,
         or you can check out my {' '}
         <Link to="/resume">resume</Link>, {' '}
         <Link to="/projects">projects</Link>, {' '}
         view <Link to="/stats">site statistics</Link>, {' '}
         or <Link to="/contact">contact</Link> me.
       </p>
-      <p> Source available <a href="https://github.com/mldangelo/personal-site">here</a>.</p>
+      <p> Source for this website available <a href="https://github.com/josephrewald/personal-site">here</a>.</p>
     </article>
   </Main>
 );
