d7f1f3ebe3537d65225505626d32fe672e7a344b
Joseph Rewald <joseph.rewald@gmail.com>
2021-05-20 23:11:36 +0000

    Regular commit.
    Resume more complete. Removed skills and courses sections.
    Will come back to those later.

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -5,20 +5,26 @@ import Main from '../layouts/Main';
 
 import Education from '../components/Resume/Education';
 import Experience from '../components/Resume/Experience';
-import Skills from '../components/Resume/Skills';
-import Courses from '../components/Resume/Courses';
+// import Skills from '../components/Resume/Skills';
+// import Courses from '../components/Resume/Courses';
 import References from '../components/Resume/References';
 
-import courses from '../data/resume/courses';
+// import courses from '../data/resume/courses';
 import degrees from '../data/resume/degrees';
 import positions from '../data/resume/positions';
-import { skills, categories } from '../data/resume/skills';
+// import { skills, categories } from '../data/resume/skills';
+
+// TODO
+// Put these back down the bottom with education  and experience.
+// Also uncomment the relevant imports.
+// <Skills skills={skills} categories={categories} />
+// <Courses data={courses} />
 
 const sections = [
   'Education',
   'Experience',
-  'Skills',
-  'Courses',
+  // 'Skills',
+  // 'Courses',
   'References',
 ];
 
@@ -42,8 +48,6 @@ const Resume = () => (
       </header>
       <Education data={degrees} />
       <Experience data={positions} />
-      <Skills skills={skills} categories={categories} />
-      <Courses data={courses} />
       <References />
 
     </article>
