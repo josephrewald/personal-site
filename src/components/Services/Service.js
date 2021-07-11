2bdd8e506b053ac67b193d8868dc6bd5a8777b75
Joseph Rewald <joseph.rewald@gmail.com>
2021-07-11 12:45:25 +1000

    Try to fix images on services page.
    But fail.

--- src/components/Services/Service.js
+++ src/components/Services/Service.js
@@ -2,15 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-const { PUBLIC_URL } = process.env; // set automatically from package.json:homepage
+// const { PUBLIC_URL } = process.env; // set automatically from package.json:homepage
 
 const Service = ({ service }) => (
   <div>
     <div className="split-left">
       <div className="centered">
-        {/* TODO: make these links work somehow. */ }
         <div className="link-to" id={`${service.serviceName}`.toLowerCase()} />
-        {/* <div className="link-to" id="cad" /> */ }
         <h3 className="serviceName">{service.serviceName}</h3>
         <p className="serviceDescription">{service.serviceDescription}</p>
         <p><Link to="/contact">Contact me for more information.</Link> </p>
@@ -18,7 +16,8 @@ const Service = ({ service }) => (
     </div>
     <div className="split-right">
       <div className="centered">
-        <img src={`${PUBLIC_URL}/images/${service.serviceName}.jpg`} alt={service.serviceName} />
+        {/* <img src={`${process.env.PUBLIC_URL}${data.image}`} alt={data.title} /> */}
+        <img src={`${process.env.PUBLIC_URL}/images/${service.serviceName}.jpg`} alt={service.serviceName} />
       </div>
     </div>
   </div>
